263242cf4eb7922010261625cf2093bdc57b129d
Korbinian Schweiger <korbinian.schweiger@cern.ch>
2020-01-28 21:10:03 +0100

    Added setup.py so it can be installed form other projects

---
+++ configreader/__init__.py
@@ -0,0 +1,3 @@
+from .base import ConfigReaderBase
+
+__all__ = ["ConfigReaderBase"]
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='ConfigReader',
+   version='0.1',
+   license="MIT",
+   description='Utiliy class when working with config files and configparser',
+   author='Korbinian Schweiger',
+   author_email='korbinian.schweiger@gmail.com',
+   packages=['configreader'],
+)
